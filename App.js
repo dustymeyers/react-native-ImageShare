3508d6133c74b8ce25dc6dca9b14aff4275a3643
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-06-01 16:06:27 -0500

    added logo image

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
+import logo from './assets/logo.png';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Image source={logo} style={{ width:305, height: 159}} />
+      
       <Text style={{color: '#888', fontSize: 18}}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>

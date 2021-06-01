298cb0d612d1668c1eefaa363bab6e1d7f580510
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-06-01 16:16:18 -0500

    added a button that triggers an alert message

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import logo from './assets/logo.png';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+// import logo from './assets/logo.png'; 
+// ^ needed if you want to source your images locally ^
 
 export default function App() {
   return (
@@ -14,7 +15,13 @@ export default function App() {
       <Text style={styles.instructions}>
         To share a photo from your phone with a friend, just press the button below!
       </Text>
-      <StatusBar style="auto" />
+
+      <TouchableOpacity
+        onPress={() => alert('Hello, world!')}
+        style={{ backgroundColor: 'blue' }}
+      >
+        <Text style={{ fontSize: 20, color: '#fff'}}>Pick a Photo</Text>
+      </TouchableOpacity>
     </View>
   );
 }
